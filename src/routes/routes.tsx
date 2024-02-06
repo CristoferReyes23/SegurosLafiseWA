1f62a579dbee7780bed2851a8393b85353187448
KevinKaliH <hernandezgomezk122@gmail.com>
2024-02-05 19:06:55 -0600

    removed unnecessary context

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,6 @@
 import {
   Navigate,
+  Outlet,
   Route,
   createBrowserRouter,
   createRoutesFromElements,
@@ -8,16 +9,17 @@ import { CreatePolicy } from "@modules/Policy/pages";
 import { QuotePage } from "@modules/Quote/pages";
 import { Dashboard } from "@modules/Dashboard/views";
 import Unauthorized from "@/views/Unauthorized";
-import { RootLayout } from "@/components/layouts";
-import ProtectedLayout from "@/components/layouts/ProtectedLayout";
+// import ProtectedLayout from "@/components/layouts/ProtectedLayout";
 import { unauthorizedLoader } from "@/routes/loaders/unauthorized.loader";
+import RootView from "@/views/RootView";
+import { protectedRouteLoader } from "@/routes/loaders/protectedRoute.loader";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route id="root" path="/" element={<RootLayout />} />
+      <Route id="root" path="/" element={<RootView />} />
 
-      <Route element={<ProtectedLayout />}>
+      <Route element={<Outlet />} loader={protectedRouteLoader}>
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="policy" element={<CreatePolicy />} />
         <Route path="quote" element={<QuotePage />} />
