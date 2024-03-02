53a7c42c0b2e5aff0b4475c4104c7331d6c463e7
KevinKaliH <hernandezgomezk122@gmail.com>
2024-03-02 03:37:51 -0600

    modified templates

--- src/modules/Printer/components/FormSearchClient.tsx
+++ src/modules/Printer/components/FormSearchClient.tsx
@@ -1,9 +1,7 @@
+import CommonSelectGroup from "@/modules/Policy/components/CommonSelectGroup";
 import FormCard from "@/shared/components/FormCard";
-import FormControlTemplate from "@/shared/components/Forms/FormControlTemplate";
-import FormSelectTemplate from "@/shared/components/Forms/FormSelectTemplate";
-import useFetch from "@/shared/hooks/useFetch";
-import { BaseListDataModel } from "@/shared/models/baseListData.model";
-import { FormikComponentProps, getFormikErrorField, getFormikProps } from "@/shared/utils/getFormikProps";
+import GroupInputForm from "@/shared/components/Forms/GroupInputForm";
+import { FormikComponentProps } from "@/shared/utils/getFormikProps";
 import { EnumUrlCatalogsPaths } from "@/shared/utils/urlPaths";
 
 const FormSearchClient = ({ form }: FormikComponentProps) => {
@@ -11,27 +9,29 @@ const FormSearchClient = ({ form }: FormikComponentProps) => {
     <FormCard title="Buscar cliente">
       <form noValidate onSubmit={form.handleSubmit}>
         <div className="row">
-          <div className="col position-relative">
-            <div className="input-group has-validation">
-              <label className="input-group-text">Tipo de Identificación</label>
-              <SelectTypeId form={form} />
-            </div>
+          <div className="col">
+            <CommonSelectGroup
+              name="typeUserId"
+              form={form}
+              firsOption="Seleccione un tipo de identificación"
+              label="Tipo de identificación"
+              urlPath={EnumUrlCatalogsPaths.typeId}
+            />
           </div>
 
-          <div className="col position-relative">
-            <div className="input-group has-validation">
-              <FormControlTemplate
-                errorMessage={getFormikErrorField(form, "userIdValue")}
-                placeholder="Identificación"
-                {...getFormikProps(form, "userIdValue")}
-              />
-              <button className="btn btn-primary" type="submit" disabled={form.isSubmitting || !form.isValid}>
-                {form.isSubmitting && (
-                  <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-                )}
-                <i className="fa-solid fa-magnifying-glass"></i> Buscar
-              </button>
-            </div>
+          <div className="col">
+            <GroupInputForm formik={form} label="Identificación" name="userIdValue" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-auto d-flex align-items-center">
+            <button className="btn btn-primary" type="submit" disabled={form.isSubmitting || !form.isValid}>
+              {form.isSubmitting && (
+                <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+              )}
+              <i className="fa-solid fa-magnifying-glass"></i> Buscar
+            </button>
           </div>
         </div>
       </form>
@@ -40,25 +40,3 @@ const FormSearchClient = ({ form }: FormikComponentProps) => {
 };
 
 export default FormSearchClient;
-
-const SelectTypeId = ({ form }: FormikComponentProps) => {
-  const { data } = useFetch<BaseListDataModel[]>({
-    to: "LAFISE",
-    urlPath: EnumUrlCatalogsPaths.typeId,
-  });
-
-  return (
-    <FormSelectTemplate
-      data={
-        data ?? [
-          { id: "5", text: "Ruc" },
-          { id: "1", text: "Cédula" },
-          { id: "2", text: "Etc" },
-        ]
-      }
-      {...getFormikProps(form, "typeUserId")}
-      errorMessage={getFormikErrorField(form, "typeUserId")}
-      firstOptionEmpty="Seleccione un tipo de identificación"
-    />
-  );
-};
