79d11ead353db8185928fda99092f256237b8bfb
KevinKaliH <hernandezgomezk122@gmail.com>
2024-03-03 12:21:16 -0600

    workin on template and refactoring tabs

--- src/modules/Policy/components/Steps/PlanPolicy.tsx
+++ src/modules/Policy/components/Steps/PlanPolicy.tsx
@@ -1,28 +1,118 @@
 import { PlanSelect } from "@/modules/Policy/components/PlanSelect";
-import { FormikComponentProps, getFormikProps } from "@/shared/utils/getFormikProps";
+import { FormikComponentProps } from "@/shared/utils/getFormikProps";
 import FormCard from "@/shared/components/FormCard";
-import { FloatingLabel } from "react-bootstrap";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
-import Form from "react-bootstrap/esm/Form";
+import { CommonSelectWithDependency } from "@/shared/components/Forms/Selects/CommonSelectWithDependency";
+import { EnumUrlCatalogsPaths } from "@/shared/utils/urlPaths";
+import CommonSelectGroup from "@/shared/components/Forms/Selects/CommonSelectGroup";
+import GroupInputForm from "@/shared/components/Forms/GroupInputForm";
+import { ConstRegex } from "@/shared/utils/constRegex";
+import { useEffect, useState } from "react";
 
 const PlanPolicy = ({ form }: FormikComponentProps) => {
   return (
     <div>
-      <FormCard title="Planes de póliza a solicitar">
+      <FormCard title="Datos de cotización del vehículo">
         <Row>
-          <Col>
+          <Col sm={4}>
             <PlanSelect form={form} />
           </Col>
-          <Col>
-            <FloatingLabel label={"Moneda"}>
-              <Form.Control {...getFormikProps(form, "moneda")} disabled placeholder="" />
-            </FloatingLabel>
+
+          <Col sm={4}>
+            <CommonSelectGroup
+              form={form}
+              name="usoo"
+              nameText="xusoo"
+              urlPath={EnumUrlCatalogsPaths.uses}
+              label="Uso del vehículo"
+              firsOption="Seleccione un tipo de uso"
+            />
+          </Col>
+
+          <Col sm={4}>
+            <VehiclePrice form={form} />
           </Col>
         </Row>
+
+        <Row>
+          <Col sm={4}>
+            <CommonSelectWithDependency
+              form={form}
+              name={"marcaId"}
+              dependencyField="planId"
+              nameText="xmarca"
+              pathApi={EnumUrlCatalogsPaths.brands}
+              floatingLabel={"Marca"}
+              firstOptionEmpty="Seleccione una marca"
+            />
+          </Col>
+          <Col sm={4}>
+            <CommonSelectWithDependency
+              form={form}
+              name={"modeloId"}
+              nameText="xmodelo"
+              dependencyField={"marcaId"}
+              pathApi={EnumUrlCatalogsPaths.models}
+              floatingLabel={"Modelo"}
+              firstOptionEmpty="Seleccione un modelo"
+            />
+          </Col>
+
+          <Col sm={4}>
+            <CommonSelectWithDependency
+              form={form}
+              name={"anio"}
+              dependencyField={"modeloId"}
+              pathApi={EnumUrlCatalogsPaths.years}
+              floatingLabel={"Año"}
+              firstOptionEmpty="Seleccione una marca"
+            />
+          </Col>
+        </Row>
+      </FormCard>
+
+      <FormCard title="Detalle del vehículo" classNames="mt-3">
+        <div className="form-fields-container">
+          <GroupInputForm formik={form} label="Número de placa" name="placa" />
+          <GroupInputForm formik={form} label="Número de motor" name="motor" />
+          <GroupInputForm formik={form} label="Número de chasis" name="chasis" />
+          <GroupInputForm formik={form} label="Color" name="color" />
+          <GroupInputForm
+            disabled={true}
+            readOnly={true}
+            formik={form}
+            label="Número de puertas"
+            name="puertas"
+            type="text"
+            maxLength={2}
+          />
+        </div>
       </FormCard>
     </div>
   );
 };
 
 export default PlanPolicy;
+
+export const VehiclePrice = ({ form }: FormikComponentProps) => {
+  const [textComplement, setTextComplement] = useState("");
+
+  useEffect(() => {
+    const currency = form.values["moneda"];
+
+    if (currency) {
+      setTextComplement(" en " + currency);
+    } else setTextComplement("");
+  }, [form.values["moneda"]]);
+
+  return (
+    <GroupInputForm
+      type="text"
+      formik={form}
+      label={`Precio de compra del vehículo ${textComplement}`}
+      name="valorNuevo"
+      regexValidation={ConstRegex.onlyNumbers}
+    />
+  );
+};
--- src/modules/Policy/components/Steps/VehicleForm.tsx
+++ src/modules/Policy/components/Steps/VehicleForm.tsx
@@ -2,10 +2,6 @@ import GroupInputForm from "@/shared/components/Forms/GroupInputForm";
 import FormCard from "@/shared/components/FormCard";
 import { FormikProps, FormikValues } from "formik/dist/types";
 import Stack from "react-bootstrap/esm/Stack";
-import { CommonSelectWithDependency } from "@/shared/components/Forms/Selects/CommonSelectWithDependency";
-import { EnumUrlCatalogsPaths } from "@/shared/utils/urlPaths";
-import CommonSelectGroup from "@/shared/components/Forms/Selects/CommonSelectGroup";
-import { ConstRegex } from "@/shared/utils/constRegex";
 
 interface Props {
   form: FormikProps<FormikValues>;
@@ -15,65 +11,19 @@ const VehicleForm = ({ form }: Props) => {
   return (
     <div>
       <Stack gap={3}>
-        <FormCard title="Modelo del vehículo">
-          <div className="form-fields-container">
-            <CommonSelectWithDependency
-              form={form}
-              name={"marcaId"}
-              dependencyField="planId"
-              nameText="xmarca"
-              pathApi={EnumUrlCatalogsPaths.brands}
-              floatingLabel={"Marca"}
-              firstOptionEmpty="Seleccione una marca"
-            />
-            <CommonSelectWithDependency
-              form={form}
-              name={"modeloId"}
-              nameText="xmodelo"
-              dependencyField={"marcaId"}
-              pathApi={EnumUrlCatalogsPaths.models}
-              floatingLabel={"Modelo"}
-              firstOptionEmpty="Seleccione un modelo"
-            />
-            <CommonSelectWithDependency
-              form={form}
-              name={"anio"}
-              dependencyField={"modeloId"}
-              pathApi={EnumUrlCatalogsPaths.years}
-              floatingLabel={"Año"}
-              firstOptionEmpty="Seleccione una marca"
-            />
-            <GroupInputForm
-              type="text"
-              formik={form}
-              label="Precio del vehículo nuevo"
-              name="valorNuevo"
-              regexValidation={ConstRegex.onlyNumbers}
-            />
-          </div>
-        </FormCard>
-
         <FormCard title="Datos del vehículo">
           <div className="form-fields-container">
-            <CommonSelectGroup
-              form={form}
-              name="usoo"
-              nameText="xusoo"
-              urlPath={EnumUrlCatalogsPaths.uses}
-              label="Uso del vehículo"
-              firsOption="Seleccione un tipo de uso"
-            />
             <GroupInputForm formik={form} label="Número de placa" name="placa" />
             <GroupInputForm formik={form} label="Número de motor" name="motor" />
             <GroupInputForm formik={form} label="Número de chasis" name="chasis" />
             <GroupInputForm formik={form} label="Color" name="color" />
             <GroupInputForm
+              disabled={true}
               formik={form}
               label="Número de puertas"
               name="puertas"
               type="text"
               maxLength={2}
-              regexValidation={ConstRegex.onlyNumberDigit}
             />
           </div>
         </FormCard>
